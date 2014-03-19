b710c6051a76bb152a07caf26fcc2c4106dccb7b
Sergio Bello <alu0100831887@ull.edu.es>
2014-03-19 14:10:17 +0000

    Practica 7

---
+++ usomodulo.py
@@ -0,0 +1,8 @@
+import modulo
+t_upla=(10,100,1000,10000,100000,1000000,10000000,100000000)
+  for i in range(0, len(t_upla[i])):
+    print modulo.aproximacion(t_upla[i])
+
+# El numero maximo de elmentos de la t_upla es de 8
+# se producen para 9 o mas elementos
+# No
